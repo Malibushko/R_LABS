a1da40f5e10745960a62ad61137c8867fe5aff84
Egor Tsyba <egor.tsyba@nixsolutions.com>
2021-03-21 13:06:32 +0200

    First laba

---
+++ Foo.r
@@ -0,0 +1,28 @@
+split_data <- function(dataset, ratio = 0.75) {
+    data_size <- nrow(dataset)
+    sample <- sample.int(n = data_size, size = floor(ratio * data_size))
+    list(train = dataset[sample, ], test = dataset[-sample, ])
+}
+
+numerate_classes <- function(dataset) {
+    for (i in seq_len(ncol(dataset))) {
+        if (is.character(dataset[, i])) {
+            dataset[, i] <- as.numeric(as.factor(dataset[, i]))
+        }
+    }
+    dataset
+}
+
+fill_missing <- function(dataset, na_character = "?") {
+    for (i in seq_len(ncol(dataset))) {
+        if (is.numeric(dataset[, i]) || is.logical(dataset[, i]))
+          dataset[, i][is.na(dataset[, i])] <- mean(dataset[, i], na.rm = TRUE)
+    }
+    dataset
+}
+
+cleve <- read.table("Datasets/cleve.data", na.strings = c("?"))
+iris <- read.csv("Datasets/iris.data");
+
+cleve <- fill_missing(cleve)
+cleve
